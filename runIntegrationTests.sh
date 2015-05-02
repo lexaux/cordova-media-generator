b49bef41a5120c96bc75eb1de6fd0fd97cac7ea6
Martin Reinhardt <contact@martinreinhardt-online.de>
2015-05-02 12:27:42 +0200

    prepare integration testing

---
+++ runIntegrationTests.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+cwd=$(pwd)
+cd ..  
+rm -rf cordova-test-app
+git clone https://github.com/hypery2k/cordova-demo-app.git cordova-test-app
+cd cordova-test-app
+npm install
+bower install
+grunt "ci:$PLATFORM" "$PLATFORM"
+mkdir icons
+cp ../cordova-media-generator/images/* icons/
+cp ../cordova-media-generator/mediagen-config.json .
+cordova-media-gen
+echo "Changing back to plugin directy: "$cwd
+cd $cwd
